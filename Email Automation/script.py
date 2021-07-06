8206f2001680921efbcf6fa906592d05a6710dad
twilight-warlock <devansh.ts@somaiya.edu>
2021-07-06 14:18:24 +0530

    Automate emailing process

---
+++ Email Automation/script.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from_addr=' '
+to_addr=['abc@gmail.com']
+
+msg=MIMEMultipart()
+
+msg['From']=from_addr
+msg['To']=" ,".join(to_addr)
+msg['subject']='just to check'
+
+body='hello world'
+
+email=' '
+password=' '
+
+mail=smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login(email,password)
+text=msg.as_string()
+mail.sendmail(from_addr,to_addr,text)
+mail.quit()
